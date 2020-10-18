6cecfb2cf26e1a45884b83ee61b72eb2a537252c
Khant Thurein Han <khantthurein@student.unimelb.edu.au>
2020-10-18 23:35:52 +1100

    Modified player values

--- Assets/Scripts/BounceHorizontal.cs
+++ Assets/Scripts/BounceHorizontal.cs
@@ -7,6 +7,8 @@ public class BounceHorizontal : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField]
     private GameObject tower;
+    //Speed loss determines how much speed is lost when bouncing, a higher value will have a bouncier result
+    public float speedLoss = 0.7f;
     private Collider collider;
     void Start()
     {
@@ -14,13 +16,9 @@ public class BounceHorizontal : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other) {
         Vector3 av = tower.GetComponent<Rigidbody>().angularVelocity;
-        tower.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, av.y * -0.7f, 0);
+
+        tower.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, av.y * -speedLoss, 0);
     }
 }
