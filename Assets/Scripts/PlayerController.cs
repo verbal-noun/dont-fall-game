985efe885f493c7beb85f14acf24200ca6c9efb7
Khant Thurein Han <khantthurein@student.unimelb.edu.au>
2020-10-27 02:34:16 +1100

    Implemented numerous QoL changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,25 +22,22 @@ public class PlayerController : MonoBehaviour
     public float jumpBuffer = 0.1f;
     private Rigidbody rb;
     private bool grounded;
-
     public float turnSpeed = 10f;
-
     public float walkSpeed = 3f;
+
+    public float wallBuffer = 1f;
     public bool walkEnabled = true;
-
     private Animator animator;
-
     private AudioManager audio;
-
     private bool btnDownJump = false;
     private bool btnJump = false;
     private bool btnUpJump = false;
-
     private bool isOnGround = false;
+    private Rigidbody trb;
     public GameObject GetTower(){
         return tower;
     }
-    void Awake()
+    void Start()
     {
         animator = GetComponent<Animator>();
         playerCollider = GetComponent<BoxCollider>();
@@ -48,6 +45,8 @@ public class PlayerController : MonoBehaviour
         audio = FindObjectOfType<AudioManager>();
         powerbar = FindObjectOfType<Powerbar>();
         character = transform.Find("Character").gameObject;
+        trb = tower.GetComponent<Rigidbody>();
+
 
         powerbar.SetPower(0);
         powerbar.SetMaxValue(maxJumpPower);
@@ -58,16 +57,15 @@ public class PlayerController : MonoBehaviour
         btnDownJump = Input.GetButtonDown("Jump");
         btnJump = Input.GetButton("Jump");
         btnUpJump = Input.GetButtonUp("Jump");
-
     }
 
     void Jump()
     {
         audio.PlayJump(jumpPower/maxJumpPower);
-
         rigidbody.velocity += Vector3.up * jumpPower;
-        if (Mathf.Sign(Input.GetAxis("Horizontal")) == Mathf.Sign(direction)){
-            tower.GetComponent<Rigidbody>().AddTorque(Vector3.up * direction * angularSpeed, ForceMode.VelocityChange);
+        //Debug.Log(Input.GetAxis("Horizontal") + "____"  + direction);
+        if (Input.GetAxis("Horizontal") == Mathf.Sign(direction)){
+            trb.AddTorque(Vector3.up * direction * angularSpeed, ForceMode.VelocityChange);
         }
         animator.SetBool("Grounded", false);
     }
@@ -76,7 +74,6 @@ public class PlayerController : MonoBehaviour
         float facing = Input.GetAxis("Horizontal");
         if (facing > 0)
         {
-
             direction = 1;
         }
         else if (facing < 0)
@@ -100,32 +97,32 @@ public class PlayerController : MonoBehaviour
         grounded = isGrounded();
         animator.SetBool("Grounded", grounded);
         animator.SetFloat("Vertical Speed", rigidbody.velocity.y);
-        animator.SetFloat("Horizontal Speed", Mathf.Abs(tower.GetComponent<Rigidbody>().angularVelocity.y));
-
+        animator.SetFloat("Horizontal Speed", Mathf.Abs(trb.angularVelocity.y));
         animator.SetBool("isCharging", Input.GetButton("Jump"));
 
         float h = Input.GetAxis("Horizontal");
+        //Debug.Log(CheckWall());
 
         if (grounded)
         {   
             if (isOnGround && walkEnabled){
-                if (h != 0 && Mathf.Sign(h) == direction){
-                    tower.GetComponent<Rigidbody>().AddTorque(Vector3.up * h * walkSpeed * 0.001f, ForceMode.VelocityChange);
+                if (!CheckWall() && ((h < 0f && Mathf.Round(trb.angularVelocity.y) <= 0f) || (h > 0f && Mathf.Round(trb.angularVelocity.y) >= 0f))){
+                    //trb.AddTorque(Vector3.up * h * walkSpeed * 0.001f, ForceMode.VelocityChange);
+                    trb.angularVelocity = Mathf.Sign(h) * Vector3.up * walkSpeed * 0.1f;
                 }
                 else {
-                    tower.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                    trb.angularVelocity = Vector3.zero;
                 }
             }
             if (btnDownJump)
             {
-                tower.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                trb.angularVelocity = Vector3.zero;
                 jumpPower = 0;
                 audio.Play("JumpCharge");
-
             }
             else if (btnJump)
             {
-                tower.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                trb.angularVelocity = Vector3.zero;
                 jumpPower += Time.deltaTime * jumpChargeSpeed;
                 if (jumpPower > maxJumpPower)
                 {
@@ -136,75 +133,54 @@ public class PlayerController : MonoBehaviour
             {
                 if (jumpPower > minJumpThreshold)
                 {
+                    trb.angularVelocity = Vector3.zero;
                     Jump();
                 }
-
                 jumpPower = 0;
             }
         }
         powerbar.SetPower(jumpPower);
-        //Debug.Log("Jumping Power: " + jumpPower);
     }
-
-
     //Ground check
     bool isGrounded()
     {
-
-        RaycastHit hit;
         Collider pc = playerCollider;
-        
-        // //Center
-        // bool ray1 = Physics.Raycast(new Vector3(pc.bounds.center.x - pc.bounds.extents.x, pc.bounds.center.y, pc.bounds.center.z), Vector3.down, out hit, pc.bounds.extents.y + jumpBuffer);
-        // bool ray2 = Physics.Raycast(new Vector3(pc.bounds.center.x, pc.bounds.center.y, pc.bounds.center.z), Vector3.down, out hit, pc.bounds.extents.y + jumpBuffer);
-        // bool ray3 = Physics.Raycast(new Vector3(pc.bounds.center.x + pc.bounds.extents.x, pc.bounds.center.y, pc.bounds.center.z), Vector3.down, out hit, pc.bounds.extents.y + jumpBuffer);
 
-        // //Back
-        // bool ray4 = Physics.Raycast(new Vector3(pc.bounds.center.x - pc.bounds.extents.x, pc.bounds.center.y, pc.bounds.center.z + pc.bounds.extents.z), Vector3.down, out hit, pc.bounds.extents.y + jumpBuffer);
-        // bool ray5 = Physics.Raycast(new Vector3(pc.bounds.center.x, pc.bounds.center.y, pc.bounds.center.z + pc.bounds.extents.z), Vector3.down, out hit, pc.bounds.extents.y + jumpBuffer);
-        // bool ray6 = Physics.Raycast(new Vector3(pc.bounds.center.x + pc.bounds.extents.x, pc.bounds.center.y, pc.bounds.center.z + pc.bounds.extents.z), Vector3.down, out hit, pc.bounds.extents.y + jumpBuffer);
+        Bounds below = new Bounds();
+        below.center = new Vector3(pc.bounds.center.x, pc.bounds.center.y - jumpBuffer, pc.bounds.center.z);
+        below.extents = pc.bounds.extents;
 
-
-        // //Front
-        // bool ray7 = Physics.Raycast(new Vector3(pc.bounds.center.x - pc.bounds.extents.x, pc.bounds.center.y, pc.bounds.center.z - pc.bounds.extents.z), Vector3.down, out hit, pc.bounds.extents.y + jumpBuffer);
-        // bool ray8 = Physics.Raycast(new Vector3(pc.bounds.center.x, pc.bounds.center.y, pc.bounds.center.z - pc.bounds.extents.z), Vector3.down, out hit, pc.bounds.extents.y + jumpBuffer);
-        // bool ray9 = Physics.Raycast(new Vector3(pc.bounds.center.x + pc.bounds.extents.x, pc.bounds.center.y, pc.bounds.center.z - pc.bounds.extents.z), Vector3.down, out hit, pc.bounds.extents.y + jumpBuffer);
-
-        bool isHit = Physics.CheckBox(new Vector3(pc.bounds.center.x, pc.bounds.center.y - jumpBuffer, pc.bounds.center.z), pc.bounds.extents, transform.rotation, platformLayerMask);
+        bool isHit = Physics.CheckBox(below.center, below.extents, transform.rotation, platformLayerMask);
         //Debug.Log(isGrounded);
         return isHit;
     }
 
-    // private void OnDrawGizmos() {
-    //     RaycastHit hit;
-    //     Gizmos.color = Color.red;
-    //     //bool ray1 = Physics.Raycast(new Vector3(pc.bounds.center.x - pc.bounds.extents.x, pc.bounds.center.y , pc.bounds.center.z), Vector3.down,  out hit, pc.bounds.extents.y + jumpBuffer);
-    //     Gizmos.DrawRay(new Vector3(pc.bounds.center.x - pc.bounds.extents.x, pc.bounds.center.y , pc.bounds.center.z), Vector3.down * (1+jumpBuffer));
-    //      Gizmos.DrawRay(new Vector3(pc.bounds.center.x, pc.bounds.center.y , pc.bounds.center.z), Vector3.down * (1+jumpBuffer));
-    //       Gizmos.DrawRay(new Vector3(pc.bounds.center.x + pc.bounds.extents.x, pc.bounds.center.y , pc.bounds.center.z), Vector3.down * (1+jumpBuffer));
-
-    //     //bool ray2 = Physics.Raycast(new Vector3(pc.bounds.center.x, pc.bounds.center.y, pc.bounds.center.z),  Vector3.down,  out hit, pc.bounds.extents.y + jumpBuffer);
-    //     //bool ray3 = Physics.Raycast(new Vector3(pc.bounds.center.x + pc.bounds.extents.x , pc.bounds.center.y , pc.bounds.center.z), Vector3.down,  out hit, pc.bounds.extents.y + jumpBuffer);
-
-    // }
     private void OnCollisionEnter(Collision other)
     {
-        //Debug.Log("Collison! Platform!");
-        Vector3 av = tower.GetComponent<Rigidbody>().angularVelocity;
-        if (isGrounded()){
+        if (isGrounded() && !CheckWall()){
             isOnGround = true;
-            tower.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
         }
         else{
             audio.PlayOneShot("Hit");
         }
-        
     }
 
     private void OnCollisionExit(Collision other) {
-        if (isGrounded()){
+        if (isGrounded() && rb.velocity.y > 0f){
             isOnGround = false;
         }
+    }
+
+    bool CheckWall(){
+        Collider pc = playerCollider;
+
+        Bounds dir = new Bounds();
+        dir.center = new Vector3(pc.bounds.center.x + (wallBuffer * direction), pc.bounds.center.y + jumpBuffer, pc.bounds.center.z);
+        dir.extents = pc.bounds.extents;
+
+        bool isHit = Physics.CheckBox(dir.center, dir.extents, transform.rotation, platformLayerMask);
+        
+        return isHit;
     }
     public void PlayLand(){
         audio.PlayOneShot("Land");
