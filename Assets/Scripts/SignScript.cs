cca1963a4829206a8839c48d916b01b273ddda5a
Khant Thurein Han <khantthurein@student.unimelb.edu.au>
2020-10-30 03:21:41 +1100

    Implemented dialog signs in cylinder scene

--- Assets/Scripts/SignScript.cs
+++ Assets/Scripts/SignScript.cs
@@ -7,7 +7,7 @@ public class SignScript : MonoBehaviour
     private Collider collider;
     private bool active = false;
     public Dialog dialog;
-
+    private bool inbox = false;
     public string[] sentences;
     void Start()
     {
@@ -18,18 +18,24 @@ public class SignScript : MonoBehaviour
         if (active && Input.GetKeyDown(KeyCode.E)){
             dialog.SetSentences(sentences);
             dialog.StartDialog();
+            active = false;
+        }
+
+        if (!dialog.transform.Find("TextBox").gameObject.activeSelf && inbox){
+            active = true;
         }
     }
     private void OnTriggerEnter(Collider other) {
         if (other.tag == "Player"){
             active = true;
+            inbox = true;
         }
-
     }
 
     private void OnTriggerExit(Collider other) {
         if (other.tag == "Player"){
             active = false;
+            inbox = false;
         }
     }
 }
