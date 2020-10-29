96eb167064c9a902225fbbf997089bf5ed4f6215
Khant Thurein Han <khantthurein@student.unimelb.edu.au>
2020-10-30 02:58:51 +1100

    Implementing dialog system

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -3,24 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+// Following tutorial from https://www.youtube.com/watch?v=f-oSXg6_AMQ
 public class Dialog : MonoBehaviour
 {
     // Start is called before the first frame update
     public string[] sentences;
-    
-    private TextMeshProUGUI text;
+    public float typingSpeed = 0.02f;
     [SerializeField]
+    private TextMeshProUGUI text;
+    
     private int index;
-    void Start()
+
+    public GameObject continueButton;
+    public GameObject textbox;
+    public void StartDialog()
     {
+        if (textbox.activeSelf == true) return;
+        textbox.SetActive(true);
         StartCoroutine(Type());
     }
 
+    void Update(){
+        if (text.text.Equals(sentences[index])){
+            continueButton.SetActive(true);
+        }
 
+        if (continueButton.activeSelf && Input.GetKeyDown(KeyCode.E)){
+            NextSentence();
+        }
+
+    }
     IEnumerator Type(){
         foreach (char letter in sentences[index].ToCharArray()){
             text.text += letter;
-            yield return new WaitForSeconds(0.02f);
+            yield return new WaitForSeconds(typingSpeed);
         }
     }
     void NextSentence()
@@ -30,5 +46,15 @@ public class Dialog : MonoBehaviour
             text.text = "";
             StartCoroutine(Type());
         }
+        else {
+            text.text = "";
+            continueButton.SetActive(false);
+            textbox.SetActive(false);
+        }
+    }
+
+    public void SetSentences(string[] sentences){
+        this.sentences = sentences;
+        index = 0;
     }
 }
---
+++ Assets/Scripts/SignScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignScript : MonoBehaviour
+{
+    private Collider collider;
+    private bool active = false;
+    public Dialog dialog;
+
+    public string[] sentences;
+    void Start()
+    {
+        collider = GetComponent<Collider>();
+    }
+
+    void Update(){
+        if (active && Input.GetKeyDown(KeyCode.E)){
+            dialog.SetSentences(sentences);
+            dialog.StartDialog();
+        }
+    }
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player"){
+            active = true;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.tag == "Player"){
+            active = false;
+        }
+    }
+}
