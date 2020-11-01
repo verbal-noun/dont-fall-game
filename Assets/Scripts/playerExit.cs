13f59141218462744642f9be45d07a9ef4aafee3
Tony Zhou <hanyongz@student.unimelb.edu.au>
2020-11-01 20:05:25 +0800

    exit scene added

---
+++ Assets/Scripts/playerExit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerExit : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other) {
+         if (other.tag == "exit") {
+             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+         }
+     }
+}
