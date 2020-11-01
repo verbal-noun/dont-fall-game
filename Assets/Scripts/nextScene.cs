151f59f501a4556ba54f2bd9c9ddfdf677436c67
Tony Zhou <hanyongz@student.unimelb.edu.au>
2020-11-01 17:40:00 +0800

    scene transitions

---
+++ Assets/Scripts/nextScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextScene : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
