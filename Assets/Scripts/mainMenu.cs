29f72f35506db37499f4f69608ebcc4778309346
verbal-noun <kaifahsan420@gmail.com>
2020-11-02 16:41:57 +1100

    Fixed further collider bugs and sound issues

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -3,22 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class mainMenu : MonoBehaviour
-{
-    public void PlayGame()
-    
+public class MainMenu : MonoBehaviour {
+    public void PlayGame ()
+
     {
-        if (SceneManager.GetActiveScene().buildIndex == 0){
-            AudioManager.instance.Stop("Theme");
+        if (SceneManager.GetActiveScene ().buildIndex == 0) {
+            AudioManager.instance.Stop ("Theme");
         }
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
 
     }
 
-    public void QuitGame()
-    {
-        Debug.Log("QUIT!");
-        Application.Quit();
+    public void QuitGame () {
+        Debug.Log ("QUIT!");
+        Application.Quit ();
     }
 
 }
