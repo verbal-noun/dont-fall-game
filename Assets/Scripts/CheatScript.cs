b4e4d111fc6eea6c91854fa96997478805d5e4fb
verbal-noun <kaifahsan420@gmail.com>
2020-11-02 13:32:09 +1100

    Implemented cheat script for quicker testing

--- Assets/Scripts/CheatScript.cs
+++ Assets/Scripts/CheatScript.cs
@@ -1,29 +1,46 @@
 using UnityEngine;
 
 public class CheatScript : MonoBehaviour {
-    public KeyCode specialKey; 
-    public KeyCode upArrow; 
-    public KeyCode downArrow; 
+    public KeyCode specialKey;
+    public KeyCode upArrow;
+    public KeyCode downArrow;
 
-    public GameObject character; 
+    public GameObject mountain;
+    private bool cheatMode = false;
 
+    private int speed = 40;
 
-    public void Update() {
-        if(Input.GetKey(KeyCode.LeftShift)) {
-            
-            if (Input.GetKey(KeyCode.UpArrow)) {
-                this.GetComponent<PlayerController>().enabled = false;
-                this.GetComponent<BetterGravity>().enabled = false;
+    public void FixedUpdate () {
+
+        if (Input.GetKey (KeyCode.LeftShift)) {
+
+            Debug.Log ("Mode activated");
+            this.GetComponent<PlayerController> ().enabled = false;
+            this.GetComponent<BetterGravity> ().enabled = false;
+            this.GetComponent<Rigidbody> ().useGravity = false;
+
+            if (Input.GetKey (KeyCode.UpArrow)) {
                 this.transform.position += Vector3.up;
             }
 
-            if (this.GetComponent<PlayerController>().enabled == false) {
-                this.GetComponent<PlayerController>().enabled = true;
+            if (Input.GetKey (KeyCode.DownArrow)) {
+                this.transform.position -= Vector3.up;
             }
 
-            if (Input.GetKeyDown(KeyCode.DownArrow)) {
-
+            if (Input.GetKey (KeyCode.LeftArrow)) {
+                mountain.transform.Rotate (Vector3.down * speed * Time.deltaTime);
             }
+
+            if (Input.GetKey (KeyCode.RightArrow)) {
+                mountain.transform.Rotate (Vector3.up * speed * Time.deltaTime);
+            }
+
+        }
+
+        if (Input.GetKeyUp (KeyCode.LeftShift)) {
+            this.GetComponent<PlayerController> ().enabled = true;
+            this.GetComponent<BetterGravity> ().enabled = true;
+            this.GetComponent<Rigidbody> ().useGravity = true;
         }
     }
 }
